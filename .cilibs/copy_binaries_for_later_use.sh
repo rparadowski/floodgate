4288a578e10d6a9c6f8a159631aca60eb89240b9
Rafał Paradowski <rafal.paradowski@codilime.com>
2020-06-22 10:26:27 +0200

    added sudo where required

--- .cilibs/copy_binaries_for_later_use.sh
+++ .cilibs/copy_binaries_for_later_use.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "Copy binaries for later use"
-mkdir -p /floodgate/bin
-chmod 777 /floodgate/bin
+sudo mkdir -p /floodgate/bin
+sudo chmod 777 /floodgate/bin
 cp /go/src/github.com/codilime/floodgate/floodgate /floodgate/bin/floodgate
 
